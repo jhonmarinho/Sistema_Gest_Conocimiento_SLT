8c100dcdb04799e2b379ee0153c5d01174369fa7
Sebastian Mariño <jhonsebastianml@ufps.edu.co>
2022-03-16 13:30:44 -0500

    se elimina carpeta .git en accidentes_transito

--- Nueva carpeta/Reportes/Apps_nuevas/configsac.php
+++
@@ -1,6 +0,0 @@
-<?php
-    //CREDENCIALES SAC
-    $username = ('Taxi');
-    $password = ('taxi');
-    $connection_string = ('192.168.1.227:1521/sac');
-    $conectarSac = oci_connect($username, $password, $connection_string);
--- Nueva carpeta/temasbuscadorslt/configinfo.php
+++
@@ -1,4 +0,0 @@
-<?php
-    //CREDENCIALES INFO
-    $ServerName = "SRVDB_INFO_QA";
-    $conectar = sqlsrv_connect('SRVDB_INFO_QA', array("Database" => "Sist_Atenc_Gest_SLT", "UID" => "slt", "PWD" => "S1TQ4*INFO"));
